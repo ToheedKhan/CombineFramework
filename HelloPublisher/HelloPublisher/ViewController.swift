05bb0d774dd7c14064a0ff3a0f644339e6675399
Toheed Jahan Khan <toheed04@gmail.com>
2022-12-15 11:48:26 +0530

    Sending notification using publisher subscriber

--- HelloPublisher/HelloPublisher/ViewController.swift
+++ HelloPublisher/HelloPublisher/ViewController.swift
@@ -12,17 +12,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //sendingNotificationUsingNotificationCenter()
+        sendingNotificationUsingPublisherSubscriber()
+    }
+
+    func sendingNotificationUsingNotificationCenter() {
         //Imperative programming
         let notification = Notification.Name("MyNotification")
         let center = NotificationCenter.default
         
         let observer = center.addObserver(forName: notification, object: nil, queue: nil) { notification in
-            print("Notification received!")
+            print("Notification recevied!")
         }
         center.post(name: notification, object: nil)
         center.removeObserver(observer)
     }
+    
+    func sendingNotificationUsingPublisherSubscriber() {
+        let notification = Notification.Name("MyNotification")
 
-
+        let publisher = NotificationCenter.default.publisher(for: notification, object: nil)
+        //sink method returns AnyCancelable
+        let subscription = publisher.sink { _ in
+            print("Notification Recevied")
+        }
+//        subscription.cancel()
+        NotificationCenter.default.post(name: notification, object: nil)
+        
+       
+    }
 }
 
