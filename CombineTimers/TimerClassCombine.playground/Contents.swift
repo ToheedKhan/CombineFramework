f6ea7c25c84d5b97324231c6283121a341b71807
Toheed Jahan Khan <toheed04@gmail.com>
2022-12-19 13:46:47 +0530

    TimerClass demo

--- CombineTimers/TimerClassCombine.playground/Contents.swift
+++ CombineTimers/TimerClassCombine.playground/Contents.swift
@@ -1,7 +1,15 @@
 import UIKit
 import Combine
 
-
+/*
+ There are multiple modes available. common as default.
+ 
+ But other modes are default, which means it will handle the input sources that are not  NSConnection
+ objects.
+ The one that we are using is common, which handles a set of runloop modes for which you can define a
+ set of sources, timers and observers and so many different things.
+ And then the last one is tracking, which handles the responsive user interface.
+ */
 
 let subscription = Timer.publish(every: 1.0, on: .main, in: .common)
                     .autoconnect()
