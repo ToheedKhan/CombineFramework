dc8ec5de2e4ddcf466fd0569203f6c7b13c4dd59
Toheed Jahan Khan <toheed04@gmail.com>
2022-12-19 11:33:19 +0530

    Debugger as a breakpoint

--- DebuggingCombine/DebuggingEventsCombineDemo/DebuggingEventsCombineDemo/ViewController.swift
+++ DebuggingCombine/DebuggingEventsCombineDemo/DebuggingEventsCombineDemo/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
-
+import Combine
+/*
+ And now finally, we are using the debugger as a breakpoint to break on those conditions so you can
+ jump into the debugger.
+ And depending on the conditions, I would say all this started with the printing and then handle events
+ and then debuggers should be our final resort.
+ If you cannot figure out with printing the events and handling the events, then obviously debugger
+ is your final choice.
+ */
 class ViewController: UIViewController {
-
+    
+    private var cancellable: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let publisher = (1...10).publisher
+        
+        self.cancellable = publisher
+            .breakpoint(receiveOutput: { value in
+                return value > 9
+            })
+            .sink {
+            print($0)
+        }
+        
+//        self.cancellable = publisher
+//            .breakpointOnError()
+//            .sink {
+//            print($0)
+//        }
     }
-
-
 }
 
