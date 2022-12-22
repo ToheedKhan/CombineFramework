0ca880726d476bfe7d42d00438bd257949782ac8
Toheed Jahan Khan <toheed04@gmail.com>
2022-12-22 23:00:49 +0530

    Implemented Weather Model

--- WeatherApp/WeatherApp/Models/Weather.swift
+++ WeatherApp/WeatherApp/Models/Weather.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct WeatherResponse: Decodable {
+    let main: Weather
+}
+
+struct Weather: Decodable {
+    
+    let temp: Double?
+    let humidity: Double?
+    
+    /*
+     This will be useful in places where we are not able to get the weather.
+     Maybe the weather service is down or maybe there are some other issues.
+     So in those cases, we can actually return a particular weather where temp is nil and humidity is nil.
+     
+     Basically, we were not able to fetch the weather temperature and humidity also.
+     
+     So this will be useful in those cases where maybe we provided the wrong Api key or we provided the wrong
+     city name.That doesn't really exist.
+     
+     So in all of those scenarios, we can actually use the weather placeholder to just place nil values or don't display the weather at all.
+     */
+    static var placeholder: Weather {
+        return Weather(temp: nil, humidity: nil)
+    }
+    
+}
--- WeatherApp/WeatherApp/Services/Webservice.swift
+++ WeatherApp/WeatherApp/Services/Webservice.swift
@@ -7,17 +7,3 @@
 
 import Foundation
 
-struct WeatherResponse: Decodable {
-    let main: Weather
-}
-
-struct Weather: Decodable {
-    
-    let temp: Double?
-    let humidity: Double?
-    
-    static var placeholder: Weather {
-        return Weather(temp: nil, humidity: nil)
-    }
-    
-}
