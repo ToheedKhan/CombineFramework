683531bdbe8aaaaa70d60866a09a09eefc547c9e
Toheed Jahan Khan <toheed04@gmail.com>
2022-12-24 08:38:30 +0530

    Implemented Webservice

--- WeatherApp/WeatherApp/Services/Webservice.swift
+++ WeatherApp/WeatherApp/Services/Webservice.swift
@@ -6,4 +6,23 @@
 //
 
 import Foundation
+import Combine
 
+class Webservice {
+    
+    func fetchWeather(city: String) -> AnyPublisher<Weather, Error> {
+        
+        guard let url = URL(string: Constants.URLs.weather(city: city)) else {
+            fatalError("Invalid URL")
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: WeatherResponse.self, decoder: JSONDecoder())
+            .map { $0.main }
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher() //Since we are returning eraseToAnyPublisher, we are not tied up to any particular publisher.
+        
+    }
+    
+}
