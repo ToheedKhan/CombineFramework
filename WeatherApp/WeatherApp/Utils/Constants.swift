f28e91274a6f6b892a6ee252c4fd4370bb00a1f6
Toheed Jahan Khan <toheed04@gmail.com>
2022-12-26 23:00:23 +0530

    Displayed weather on the User Interface.

--- WeatherApp/WeatherApp/Utils/Constants.swift
+++ WeatherApp/WeatherApp/Utils/Constants.swift
@@ -15,6 +15,9 @@ struct Constants {
         static func weather(city: String) -> String {
             return "http://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=ef0fd9866ca027e0dca474cee84c53be&units=imperial"
         }
+//        static func weather(city: String) -> String {
+//            return "http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=ef0fd9866ca027e0dca474cee84c53be&units=imperial"
+//        }
         // celsius = metric
         
     }
--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    private var webservice: Webservice = Webservice()
+    private var cancellable: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+        self.cancellable = self.webservice.fetchWeather(city: "Houston")
+            .catch { _ in Just(Weather.placeholder) }
+            .map { weather in
+                if let temp = weather.temp {
+                    return "\(temp) ℉"
+                } else {
+                    return "Error getting weather!"
+                }
+        }.assign(to: \.text, on: self.temperatureLabel)
+        
     }
-
-
 }
 
