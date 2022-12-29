af271adaa26080c011945d05819bff9a0ff9c4ef
Toheed Jahan Khan <toheed04@gmail.com>
2022-12-29 08:17:30 +0530

    Added setPublisher method

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -18,17 +18,48 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-        self.cancellable = self.webservice.fetchWeather(city: "Houston")
-            .catch { _ in Just(Weather.placeholder) }
-            .map { weather in
-                if let temp = weather.temp {
-                    return "\(temp) ℉"
-                } else {
-                    return "Error getting weather!"
-                }
-        }.assign(to: \.text, on: self.temperatureLabel)
-        
+        setupPublishers()
+        /*self.cancellable = self.webservice.fetchWeather(city: "Houston")
+         .catch { _ in Just(Weather.placeholder) }
+         .map { weather in
+         if let temp = weather.temp {
+         return "\(temp) ℉"
+         } else {
+         return "Error getting weather!"
+         }
+         }.assign(to: \.text, on: self.temperatureLabel)
+         */
     }
+    
+    private func setupPublishers() {
+        
+    let publisher = NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self.cityTextField)
+        
+    self.cancellable = publisher.compactMap {
+            ($0.object as! UITextField).text?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+        }.debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+            .flatMap { city in
+                return self.webservice.fetchWeather(city: city)
+                    .catch { _ in Just(Weather.placeholder) }
+                /*
+                 Since we are using 'eraseToAnyPublisher'
+                 it allows you to catch some sort of error if you want to.
+                 In our case, we don't really care about the error, but if there is an error, we are simply going
+                 to return the placeholder.
+                 A placeholder is simply going to allow us to see that the weather in this case will be null and then
+                 the humidity and the temperature will be no.
+                 */
+                    .map { $0 }
+             
+        }.sink {
+            
+            if let temp = $0.temp {
+                self.temperatureLabel.text = "\(temp) ℉"
+            } else {
+                self.temperatureLabel.text = ""
+            }
+        }
+    }
+
 }
 
